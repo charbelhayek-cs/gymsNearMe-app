d53bac70b34d8126d9540c6475d513cc898d1c0b
charbelhayek-cs <charbel.elhayek@lau.edu>
2022-04-20 19:51:41 +0300

    changed the interface to a class then in the html i called the constructor and connected it to it so now in the ion-card i did a for loop looping on gyms frmo the database

--- gymProject/gyms-near-me/src/app/apis/fetch-gyms.service.ts
+++ gymProject/gyms-near-me/src/app/apis/fetch-gyms.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
-export interface Gym{
+export class Gym{// changed this one from interface to reglar class
   //it will check what does the table gym contain in the database
   gymId: string;
   gymName: string;
@@ -22,5 +22,6 @@ export class FetchGymsService {
   getAllGyms(){
     //return all the gyms from the database
     return this.http.get<[Gym]>(this.url+'getGyms.php');
+    //get of type Gym which is the interface created
   }
 }
--- gymProject/gyms-near-me/src/app/tab1/tab1.page.ts
+++ gymProject/gyms-near-me/src/app/tab1/tab1.page.ts
@@ -7,12 +7,17 @@ import { FetchGymsService, Gym } from '../apis/fetch-gyms.service';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  gym: Gym = new Gym(); //initialized the class with it's constructor it is a must
  gyms: Gym[];// the interface that i did in the fetch gyms file .ts
   constructor(private service: FetchGymsService) {}
-  ngOninit(){
+  // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
+  ngOnInit()
+  {
+
     //on start of every ionic application in this case the tab1
     this.service.getAllGyms().subscribe(response =>{//subscribe will catch the response
       this.gyms=response;
+      console.log(this.gyms);
     });
   }
 
