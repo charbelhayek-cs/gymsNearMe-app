3366f33a1859ff1dd7bb8329df2951952fdb3d28
charbelhayek-cs <charbel.elhayek@lau.edu>
2022-04-20 12:48:16 +0300

    added an interface to the .Ts file in the new Gym folder connecting to the php page to fetch the gyms info in the database

--- gymProject/gyms-near-me/src/app/apis/fetch-gyms.service.ts
+++ gymProject/gyms-near-me/src/app/apis/fetch-gyms.service.ts
@@ -3,8 +3,23 @@ import {HttpClient} from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
+export interface Gym{
+  //it will check what does the table gym contain in the database
+  gymId: string;
+  gymName: string;
+  gymLocation: string;
+  gymInfo: string;
+  gymRating: string;
+  monthlyFees: string;
+  imgURL: string;
+
+}
 export class FetchGymsService {
   private url='http://localhost/back-end/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getAllGyms(){
+    //return all the gyms from the database 
+    return this.http.get<[Gym]>(this.url+'getGyms.php');
+  }
 }
