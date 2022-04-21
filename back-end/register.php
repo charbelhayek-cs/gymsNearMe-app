b734a02f0684e21d21a6990596076e1262737c6a
charbelhayek-cs <charbel.elhayek@lau.edu>
2022-04-21 17:35:59 +0300

    added a hashing function for the code in the php file

--- back-end/register.php
+++ back-end/register.php
@@ -1,18 +1,19 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 include("db_info.php");
-$data=json_decode("file_get_contents("php:input"));
+$data=json_decode(file_get_contents("php://input"));
 
-$fullName = $_POST["fullName"];
-$userName = $_POST["userName"];
-$password = $_POST["password"];
-$email=$_POST["email"];
-$phoneNumber=$_POST["phoneNumber"];
-$location=$_POST["location"];
+$fullName = $data->fullName;
+$userName = $data->userName;
+$password = $data->password;
+$hashed_password= password_hash($password, PASSWORD_DEFAULT);
+$email=$data->email;
+$phoneNumber=$data->phoneNumber;
+$location=$data->location;
 
 // Insert variables in the database
-$query = $mysqli->prepare("INSERT INTO user(fullName, userName, password, email, phoneNumber, location) VALUES (?, ?, ?, ?, ?, ?);");
-$query->bind_param("ssssss", $fullName, $userName, $password, $email, $phoneNumber, $location);
+$query = $mysqli->prepare("INSERT INTO user(fullName, userName, hashed_password, email, phoneNumber, location) VALUES (?, ?, ?, ?, ?, ?);");
+$query->bind_param("ssssss", $fullName, $userName, $hashed_password, $email, $phoneNumber, $location);
 $query->execute();
 
 $response = [];
