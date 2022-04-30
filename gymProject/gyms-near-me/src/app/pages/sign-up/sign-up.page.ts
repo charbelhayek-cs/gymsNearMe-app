ddd9edf802098e477046e8012795039021b45a72
charbelhayek-cs <charbel.elhayek@lau.edu>
2022-04-30 19:39:20 +0300

    added a .service page which is called customize which will let the user customize their own workout plan and add it to the database

--- gymProject/gyms-near-me/src/app/pages/sign-up/sign-up.page.ts
+++ gymProject/gyms-near-me/src/app/pages/sign-up/sign-up.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UsersService, User } from 'src/app/apis/users.service';
-import {NgForm} from '@angular/forms';
+import { NgForm } from '@angular/forms';
 import { AlertController } from '@ionic/angular';
 
 @Component({
@@ -10,37 +10,37 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['./sign-up.page.scss'],
 })
 export class SignUpPage implements OnInit {
-user: User= new User();
+  user: User = new User();
 
   constructor(private router: Router, private service: UsersService, private alertCtrl: AlertController) { }
-  goToHomePage(){
+  goToHomePage() {
     this.router.navigateByUrl('/tabs/tab1');
   }
-  goToSignIn(){
+  goToSignIn() {
     this.router.navigateByUrl('/login');
   }
   //controlling the alerts method
-   async presentAlert() {
-  const alert =await this.alertCtrl.create({
-    message: 'passwords do not match, please try again',
-    buttons: ['Dismiss']
-  });
-  alert.present();
-}
+  async presentAlert() {
+    const alert = await this.alertCtrl.create({
+      message: 'passwords do not match, please try again',
+      buttons: ['Dismiss']//this is a button to dismiss the message
+    });
+    alert.present();
+  }
   // onSubmit(data){//to submit the list i will post the data that i got to here
-  onSubmit(form: NgForm){
- this.user = form.value;
- console.log(form.value);
-if(this.user.password===this.user.verifyPassword){
- this.service.addNewUsers(this.user).subscribe(response =>{
- console.log(response);
- this.router.navigateByUrl('/login');// to let the user go to the sign in page so that they can sign in their registered account
- });
-}
-else{
-  this.presentAlert();
-}
-}
+  onSubmit(form: NgForm) {
+    this.user = form.value;
+    console.log(form.value);
+    if (this.user.password === this.user.verifyPassword) {
+      this.service.addNewUsers(this.user).subscribe(response => {
+        console.log(response);
+        this.router.navigateByUrl('/login');// to let the user go to the sign in page so that they can sign in their registered account
+      });
+    }
+    else {
+      this.presentAlert();
+    }
+  }
 
 
   ngOnInit() {
