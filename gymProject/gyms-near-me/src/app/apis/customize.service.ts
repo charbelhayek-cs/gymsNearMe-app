e91acf463d78fa520b8e8cba1b38aee4fad42beb
charbelhayek-cs <charbel.elhayek@lau.edu>
2022-04-30 19:45:52 +0300

    filled the customize.service page to link it through a post api to the php page that i made

--- gymProject/gyms-near-me/src/app/apis/customize.service.ts
+++ gymProject/gyms-near-me/src/app/apis/customize.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+export class Plan{
+  planName: string;
+  planCreator: string;
+  planInfo: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomizeService {
+   private url='http://localhost/back-end/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  addPlan(plan: Plan){
+   return this.http.post(this.url+'customize.php', plan);
+  }
 }
