f39649097e98eb2e57d47e6a75981595780c313d
charbelhayek-cs <charbel.elhayek@lau.edu>
2022-05-03 16:22:45 +0300

    changed the getUsers.php folder because i am still getting some errors

--- back-end/getUsers.php
+++ back-end/getUsers.php
@@ -2,32 +2,47 @@
 include("db_info.php");
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: *');
-session_start();
-
 $data = json_decode(file_get_contents('php://input'));
  
-$userName = $data->userName;
+$username = $data->username;
 $password = $data->password;
- 
-$sql = "SELECT * FROM user WHERE username='$username' AND password='$password'";
-$query = $conn->query($sql);
- 
-if($query->num_rows>0){
-	$row = $query->fetch_array();
-	$out['message'] = 'Login Successful';
-	$out['user'] = uniqid('ang_');
-	$_SESSION['user'] = $row['memid'];
-}
-else{
-	$out['error'] = true;
-	$out['message'] = 'Invalid Login';
-}
- 
-echo json_encode($out);
- 
+// $hashed_password=password_hash($password, PASSWORD_DEFAULT);
 
+ if (empty($username)) {
+    echo("User Name is required");
+    exit();
+    }
+    else if(empty($password)){
+        echo("Password is required");
+        exit();
+    }
+    else{
+    
+        $sql = "SELECT * FROM users WHERE username = '" . $username . "'";
+        $result = mysqli_query($mysqli, $sql);
 
+        if (mysqli_num_rows($result) != 0) {
+            $raw = mysqli_fetch_assoc($result);
 
-
+            $dbusername = $raw['username'];
+            $dbpassword = $raw['password'];
+            $response = [];
+            if ($dbusername == $username && password_verify($password, $dbpassword)) {
+                $response[] = $raw;
+                $json_respnse = json_encode($response);
+                echo $json_respnse;
+                exit();
+            }   
+            else{
+                echo("Incorrect Username or password");
+                exit();
+            }
+        }
+        else{
+            echo("Incorrect Username or password");
+            exit();
+        }
+    }
+ 
 
 ?>
