c817cf928da24243d1d66331d6fd6815be748931
charbelhayek-cs <charbel.elhayek@lau.edu>
2022-05-03 16:22:14 +0300

    added the form tto the login page with references to the service page which is connected to the .php file and also added some methods in the login.ts folder to help the process

--- gymProject/gyms-near-me/src/app/apis/login.service.ts
+++ gymProject/gyms-near-me/src/app/apis/login.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http';
+export class Login{
+  username: string;
+  password: string;
+}
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  private url='http://localhost/back-end/';
+  constructor(private http: HttpClient) { }
+  login(login: Login){
+    return this.http.post(this.url +'getUsers.php', login);//i will post to the php so that i can compare there and return the object
 
-  constructor() { }
+  }
 }
--- gymProject/gyms-near-me/src/app/pages/login/login.page.ts
+++ gymProject/gyms-near-me/src/app/pages/login/login.page.ts
@@ -1,18 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import {LoginService,Login} from 'src/app/apis/login.service';
+import { NgForm } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  lg: Login=new Login();
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private service: LoginService, private alertCtrl: AlertController) { }
   goToSignUp(){
     this.router.navigate(['/sign-up']);
   }
   goToHomePage(){
     this.router.navigateByUrl('/tabs/tab1');
+  }
+  async presentAlert(s: string) {
+    const alert = await this.alertCtrl.create({
+      message: s,
+      buttons: ['Dismiss']//this is a button to dismiss the message
+    });
+    alert.present();
+  }
+  onSubmit(form: NgForm) {
+    this.lg = form.value;
+    console.log(form.value);
+      this.service.login(this.lg).subscribe(response => {
+        console.log(response);
+        this.presentAlert(response.toString());
+        form.reset();
+        this.router.navigateByUrl('/tabs/tab1');
+      });
   }
 
   ngOnInit() {
